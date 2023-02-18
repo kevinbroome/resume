86ebf20d78b2715576249e283ba9075c64797863
mybrotherjoe <kbroome88@gmail.com>
2023-02-18 10:44:20 +0000

    animation and js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 const infoBar = document.querySelector(".info-bar")
 const splashPage = document.querySelector(".splash-page")
 
-
 const shadeObserver = new IntersectionObserver(function (
   entries,
   shadeObserver
@@ -16,3 +15,24 @@ const shadeObserver = new IntersectionObserver(function (
 });
 
 shadeObserver.observe(splashPage);
+
+const skillReveal = document.querySelector(".skill-grid")
+const revealOptions = {
+  rootMargin: "-250px 0px 0px 0px"
+};
+
+const revealObserver = new IntersectionObserver(function (
+  entries,
+  revealObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      skillReveal.classList.add('skill-reveal');
+    } else {
+      skillReveal.classList.remove('skill-reveal');
+    }
+  });
+},
+  revealOptions);
+
+revealObserver.observe(splashPage);
