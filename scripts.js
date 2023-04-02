bf49af2feb7a934f3efd4fdca4cdc23e85c35f85
mybrotherjoe <kbroome88@gmail.com>
2023-04-02 09:10:50 +0100

    experience working (for now)

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 
+//info-bar shading anim
 const splashPage = document.querySelector(".splash-page")
-
-//skill grid and shading anim
 const infoBar = document.querySelector(".info-bar")
 const splashOptions = {
   rootMargin: "-200px 0px 0px 0px"
@@ -23,52 +22,16 @@ const shadeObserver = new IntersectionObserver(function (
 }, splashOptions);
 shadeObserver.observe(splashPage);
 
+//Experience grow colour
+const targetScroll = document.querySelector(".carousel-inner");
+const wrapHeight = document.querySelector(".exp-wrapper");
 
-window.addEventListener("scroll", function () {
-
-  const jobOne = document.querySelector(".job1");
-  const jobTwo = document.querySelector(".job2");
-  const jobThree = document.querySelector(".job3");
-  const jobFour = document.querySelector(".job4");
-  const jobFive = document.querySelector(".job5");
-
-  let scrolled = window.pageYOffset;
-  let rate = scrolled * -0.3;
-
-  if (window.innerWidth < 400) {
-    jobOne.style.transform = `translateX(${rate + 1100}%)`;
-    jobTwo.style.transform = `translateX(${rate + 1200}%)`;
-    jobThree.style.transform = `translateX(${rate + 1300}%)`;
-    jobFour.style.transform = `translateX(${rate + 1400}%)`;
-    jobFive.style.transform = `translateX(${rate + 1500}%)`;
-    console.log(`small ${rate}`);
-  }
-  else if (window.innerWidth < 600) {
-    jobOne.style.transform = `translateX(${rate + 900}%)`;
-    jobTwo.style.transform = `translateX(${rate + 1000}%)`;
-    jobThree.style.transform = `translateX(${rate + 1100}%)`;
-    jobFour.style.transform = `translateX(${rate + 1200}%)`;
-    jobFive.style.transform = `translateX(${rate + 1300}%)`;
-    console.log(`med-small ${rate}`);
-  }
-  else if (window.innerWidth < 900) {
-    jobOne.style.transform = `translateX(${rate + 750}%)`;
-    jobTwo.style.transform = `translateX(${rate + 800}%)`;
-    jobThree.style.transform = `translateX(${rate + 850}%)`;
-    jobFour.style.transform = `translateX(${rate + 900}%)`;
-    jobFive.style.transform = `translateX(${rate + 950}%)`;
-    console.log(`medium ${rate}`);
-  }
-  else {
-    jobOne.style.transform = `translateX(${rate + 700}%)`;
-    jobTwo.style.transform = `translateX(${rate + 750}%)`;
-    jobThree.style.transform = `translateX(${rate + 800}%)`;
-    jobFour.style.transform = `translateX(${rate + 850}%)`;
-    jobFive.style.transform = `translateX(${rate + 900}%)`;
-    console.log(`large ${rate}`);
-  }
-
-});
+targetScroll.addEventListener("scroll", () => {
+  const scrollDist = targetScroll.scrollLeft;
+  const heightAdj = scrollDist/5.8;
+  wrapHeight.style.height = `${heightAdj}px`
+  console.log(scrollDist);
+})
 
 //community stripe
 const commAnimOne = document.querySelector(".comm-anim1")
